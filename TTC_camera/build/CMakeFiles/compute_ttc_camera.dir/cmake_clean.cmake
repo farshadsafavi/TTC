07230a58e33f2b1df75719d08839f33322289084
Farshad Safavi <farshadtom2003@gmail.com>
2020-09-04 23:26:48 -0400

    Add files via upload

---
+++ TTC_camera/build/CMakeFiles/compute_ttc_camera.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compute_ttc_camera.dir/src/compute_ttc_camera.cpp.o"
+  "CMakeFiles/compute_ttc_camera.dir/src/structIO.cpp.o"
+  "compute_ttc_camera.exe"
+  "compute_ttc_camera.exe.manifest"
+  "compute_ttc_camera.pdb"
+  "libcompute_ttc_camera.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compute_ttc_camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
